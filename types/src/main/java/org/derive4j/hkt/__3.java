3df9cec70cb21653afd1276f61b2210ab4caa03b
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2016-04-28 13:14:41 +0200

    Remove coerce methods that are redundant due to subtyping

--- types/src/main/java/org/derive4j/hkt/__3.java
+++ types/src/main/java/org/derive4j/hkt/__3.java
@@ -9,11 +9,7 @@ package org.derive4j.hkt;
  */
 public interface __3<f, A, B, C> extends __2<__<f, A>, B, C> {
 
-    static <f, A, B, C> __3<f, A, B, C> coerce(__2<__<f, A>, B, C> fabc) {
-        return (__3<f, A, B, C>) fabc;
-    }
-
     static <f, A, B, C> __3<f, A, B, C> coerce(__<__<__<f, A>, B>, C> fabc) {
-        return coerce(__2.coerce(fabc));
+        return (__3<f, A, B, C>) fabc;
     }
 }
